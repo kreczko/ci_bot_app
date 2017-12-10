9de945429f4569413d9276cd63bfabeddb958dd2
Luke Kreczko <lkreczko@googlemail.com>
2017-12-10 14:16:25 +0000

    fixed indents

--- ci_bot_app/public/views.py
+++ ci_bot_app/public/views.py
@@ -19,15 +19,15 @@ blueprint = Blueprint('public', __name__, static_folder='../static')
 rclient = redis.from_url(os.environ.get("REDIS_URL"))
 
 TOPIC = "{}.test".format(os.environ['CLOUDKARAFKA_TOPIC_PREFIX'])
-    conf = {
+conf = {
         'bootstrap.servers': os.environ['CLOUDKARAFKA_BROKERS'],
         'session.timeout.ms': 6000,
         'default.topic.config': {'auto.offset.reset': 'smallest'},
         'security.protocol': 'SASL_SSL',
         'sasl.mechanisms': 'SCRAM-SHA-256',
         'sasl.username': os.environ['CLOUDKARAFKA_USERNAME'],
-        'sasl.password': os.environ['CLOUDKARAFKA_PASSWORD']
-    }
+        'sasl.password': os.environ['CLOUDKARAFKA_PASSWORD'],
+}
 
 PRODUCER = Producer(**conf)
 CONSUMER = Consumer(**conf)
